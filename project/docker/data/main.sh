971b754e0a03f958fdac599ab574accc0e82d15e
agricreations <moovendhanagriculture@gmail.com>
2024-01-01 04:59:12 +0000

    included docker dev environment

---
+++ project/docker/data/main.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Set non-interactive mode for package installations
+export DEBIAN_FRONTEND=noninteractive
+
+apt update && apt upgrade -y
+# Install Node.js and npm
+curl -fsSL https://deb.nodesource.com/setup_lts.x | bash -
+apt-get install -y nodejs
+apt-get install -y npm
+npm install -g -y grunt-cli
+npm install -y grunt-dart-sass --save-dev
+
+
+# Install PHP from Ondřej Surý's PPA
+apt-get install -y software-properties-common
+add-apt-repository -y ppa:ondrej/php
+apt-get update
+
+# Skip time zone question by echoing a default choice
+echo "12" | apt-get install -y php
+
+apt install -y php-cli
+apt install -y php-cgi
+apt install -y php-mysql
+apt install -y php-pgsql
+
+apt-get clean
+
+mv -f /home/config.json /var/www/html/project
+
